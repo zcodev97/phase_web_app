8060663f116d80ea085045202c10524fe862da89
zakria bilal <zcodev97@gmail.com>
2023-02-16 15:03:06 +0300

    add build folder

--- src/App.js
+++ src/App.js
@@ -4,38 +4,10 @@ import GaugeChart from "react-gauge-chart";
 import ReactSpeedometer from "react-d3-speedometer";
 
 function App() {
-  const [isCalled, setIsCalled] = useState(false);
-
-  const [items, setItems] = useState();
   const [loading, setLoading] = useState(false);
-
-  const [Generator0L1A, setGenerator0L1A] = useState(0);
-  const [Generator0L2A, setGenerator0L2A] = useState(0);
-  const [Generator0L3A, setGenerator0L3A] = useState(0);
-  const [Generator0L1V, setGenerator0L1V] = useState(0);
-  const [Generator0L2V, setGenerator0L2V] = useState(0);
-  const [Generator0L3V, setGenerator0L3V] = useState(0);
-
-  const [Generator1L1A, setGenerator1L1A] = useState(0);
-  const [Generator1L2A, setGenerator1L2A] = useState(0);
-  const [Generator1L3A, setGenerator1L3A] = useState(0);
-  const [Generator1L1V, setGenerator1L1V] = useState(0);
-  const [Generator1L2V, setGenerator1L2V] = useState(0);
-  const [Generator1L3V, setGenerator1L3V] = useState(0);
-
-  const [Generator2L1A, setGenerator2L1A] = useState(0);
-  const [Generator2L2A, setGenerator2L2A] = useState(0);
-  const [Generator2L3A, setGenerator2L3A] = useState(0);
-  const [Generator2L1V, setGenerator2L1V] = useState(0);
-  const [Generator2L2V, setGenerator2L2V] = useState(0);
-  const [Generator2L3V, setGenerator2L3V] = useState(0);
 
   const [generators, setGenerators] = useState(null);
   const [levelSensors, setLevelSensors] = useState(null);
-
-  const [lastUpdateDieselLevel, setLastUpdateDieselLevel] = useState("");
-
-  const [LevelSensor, setLevelSensor] = useState(0);
 
   async function connectToEsiur() {
     try {
@@ -45,36 +17,17 @@ function App() {
         { autoReconnect: true, reconnect: true }
       );
 
-      setLoading(false);
-
       if (connection !== null || connection !== undefined) {
-        setItems(connection.Items);
-
         setGenerators(connection.Generators);
         setLevelSensors(connection.LevelSensors);
-
-        console.log(generators);
-        console.log(levelSensors);
 
         connection.Generators[2].on(":L1A", () => {
           setGenerators(connection.Generators);
           console.log("modified");
         });
+
+        setLoading(false);
       }
-
-      // if (service === null || service === undefined) return;
-
-      // if (connection !== null || connection !== undefined) {
-      //   setServ(connection.Items);
-
-      //   connection.Items[3].on(":LastUpdate", () => {
-      //     // console.log(connection.Items[3].LastUpdate);
-      //     console.log(ser[3].LastUpdate.toLocaleDateString());
-      //     console.log(ser[3].LastUpdate.toLocaleTimeString("en-US"));
-      //   });
-      // }
-
-      // console.log(service);
     } catch (e) {
       // alert(e);
       console.log(e);
@@ -89,7 +42,7 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid text-center text-light mt-2 p-1 ">
-        {items === undefined ? (
+        {loading ? (
           <div className="container-fluid bg-primary text-light text-center">
             Loading....
           </div>
@@ -98,19 +51,19 @@ function App() {
             {levelSensors.map((levelSensor) =>
               DieselLevelSensor(
                 levelSensor.Name,
-                levelSensor.Volume,
+                levelSensor.Volume.toFixed(0),
                 levelSensor.LastUpdate
               )
             )}{" "}
             {generators.map((g) =>
               GeneratorChart(
                 g.Name,
-                g.L1A,
-                g.L2A,
-                g.L3A,
-                g.L1V,
-                g.L2V,
-                g.L3V,
+                g.L1A.toFixed(0),
+                g.L2A.toFixed(0),
+                g.L3A.toFixed(0),
+                g.L1V.toFixed(0),
+                g.L2V.toFixed(0),
+                g.L3V.toFixed(0),
                 g.LastUpdate
               )
             )}{" "}
