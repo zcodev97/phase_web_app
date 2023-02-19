b154a4fc67e4a5457a33967262b84aef8c736ac9
zakria bilal <zcodev97@gmail.com>
2023-02-19 20:12:13 +0300

    update drop down items for IoT devices and add graph for diesel level sensor

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -4,21 +4,70 @@ import ReactSelect from "react-select";
 import NavBar from "../components/navBar";
 import Warehouse from "../../node_modules/esiur/src/Resource/Warehouse";
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Line } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function ReportsPage() {
   const [firstDate, setFirstDate] = useState(new Date());
   const [secondDate, setSecondDate] = useState(new Date());
   const [loading, setLoading] = useState(false);
 
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
   const [dropDownItems, setDropDownItems] = useState(null);
 
+  const [Items, setItems] = useState(null);
+
+  const [volume, setVolume] = useState([]);
+  const [dateReport, setDateReport] = useState([]);
+
   let items = [];
+
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      legend: { position: "top" },
+      title: { display: true, text: "Diesel Level Sensors" },
+    },
+  };
+
+  const labels = dateReport.length === 0 ? [] : dateReport;
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Volume",
+        data: volume.length === 0 ? [] : volume,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Date",
+        data: dateReport.length === 0 ? [] : dateReport,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
 
   async function connectToEsiur() {
     try {
@@ -28,20 +77,83 @@ function ReportsPage() {
         { autoReconnect: true, reconnect: true }
       );
 
-      esiurConnection.Items.forEach((e) => {
-        items.push({
-          value: e.Name,
-          label: e.Name,
+      setItems(esiurConnection.Items);
+
+      if (items.length === 0) {
+        esiurConnection.Items.forEach((e) => {
+          items.push({
+            value: e.ModbusId,
+            label: e.Name,
+          });
         });
-      });
+      }
 
       setDropDownItems(items);
+
+      // console.log(items);
     } catch (e) {
       // alert(e);
       console.log(e);
       setLoading(false);
     }
     setLoading(false);
+  }
+
+  async function GetReport() {
+    // console.log(selectedItem);
+
+    const item = Items.find((h) => h.ModbusId === selectedItem);
+    console.log(item);
+
+    let result = await item.GetRecords(firstDate, secondDate, 0, 50);
+
+    // console.log(result);
+
+    let dates = result.map(
+      (item) =>
+        new Date(item.Time).toLocaleDateString() +
+        " " +
+        new Date(item.Time).toLocaleTimeString()
+    );
+
+    setDateReport(dates);
+
+    let TextVolumes = result.map((item) => item.Volume.toFixed(0));
+
+    // // Convert the data to an array of numbers
+    // const volumes = TextVolumes.map(Number);
+
+    // // Calculate the mean and standard deviation of the data
+    // const mean = volumes.reduce((acc, val) => acc + val, 0) / volumes.length;
+    // const sd = Math.sqrt(
+    //   volumes.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) /
+    //     volumes.length
+    // );
+
+    // // Filter out values that have a Z-score greater than 3 (i.e. 3 standard deviations from the mean)
+    // const filteredData = volumes.filter(
+    //   (val) => Math.abs((val - mean) / sd) <= 3
+    // );
+
+    let myData = filterMyData(result);
+
+    console.log(myData);
+    setVolume(myData);
+
+    // console.log(volumes);
+    // console.log(filteredData);
+  }
+
+  function filterMyData(results) {
+    let rt = [];
+    for (var i = 0; i < results.length - 1; i++) {
+      rt.push(
+        Math.abs(Number(results[i].Volume) - Number(results[i + 1].Volume)) /
+          (Math.abs(results[i].Time - results[i + 1].Time) / 1000)
+      );
+    }
+
+    return rt;
   }
 
   useEffect(() => {
@@ -82,17 +194,27 @@ function ReportsPage() {
         <div className="col-md-3">
           <div className="container mt-2 mb-2 p-1 text-dark  border border-2  rounded">
             <ReactSelect
-              defaultValue={selectedOption}
-              onChange={setSelectedOption}
+              defaultValue={selectedItem}
+              onChange={(opt) => setSelectedItem(opt.value)}
               options={dropDownItems}
             />
           </div>
         </div>
         <div className="col-md-3">
-          <div className="container btn mt-2 mb-2 p-1  border border-2 border-primary text-primary  rounded">
+          <div
+            className="container btn mt-2 mb-2 p-1  border border-2 border-primary text-primary  rounded"
+            onClick={GetReport}
+          >
             Get Report
           </div>
         </div>
+      </div>
+
+      <div
+        className="container bg-light text-center border border-2 border-danger"
+        style={{ height: 300 }}
+      >
+        <Line options={options} data={data} />
       </div>
     </>
   );
